c82c366491ff47c7e0cf106f6e338fe14dd3e554
DisonGo <jokerj224@gmail.com>
2021-04-26 00:25:51 +0300

    Huge performance optimisation(Fixed overusing of Two.js 'bind')

--- js/main.js
+++ js/main.js
@@ -6,27 +6,27 @@ let params = {
     autostart: true
 }
 
-let cPos = new Dot(0,0)
-    cPos.checkPos =  function (e) {
-        let x, y
-        // console.log(arguments);
-        if (typeof arguments[1] === "undefined") {
-            x = e.clientX - e.target.getBoundingClientRect().x
-            y = e.clientY - e.target.getBoundingClientRect().y
-        } else {
-            x = e.clientX - arguments[1].getBoundingClientRect().x
-            y = e.clientY - arguments[1].getBoundingClientRect().y
-        }
-        this.x = x
-        this.y = y
+let cPos = new Dot(0, 0)
+cPos.checkPos = function (e) {
+    let x, y
+    // console.log(arguments);
+    if (typeof arguments[1] === "undefined") {
+        x = e.clientX - e.target.getBoundingClientRect().x
+        y = e.clientY - e.target.getBoundingClientRect().y
+    } else {
+        x = e.clientX - arguments[1].getBoundingClientRect().x
+        y = e.clientY - arguments[1].getBoundingClientRect().y
     }
+    this.x = x
+    this.y = y
+}
 let centerP = new Dot(aWidth / 2, aHeight / 2),
     vec = new Vector(centerP, new Dot(aWidth / 2, aHeight / 2 + 10), centerP),
     ancVec = new Vector().clone(vec),
     testRotVec = new Vector(new Dot(aWidth / 2 + 100, aHeight / 2 + 100), new Dot(aWidth / 2 + 20, aHeight / 2 + 10), new Dot(aWidth / 2 - 20, aHeight / 2 - 10))
-    deTwo = new Two(params)
+deTwo = new Two(params)
 
-    // vec.sysBegChangble = false 
+// vec.sysBegChangble = false 
 
 deTwo.frameCount = 60
 deTwo.appendTo(mainCan)
@@ -73,22 +73,25 @@ rect.anchorPnts.visible = false
 star.anchorPnts.fill = createGradient(deTwo)
 
 star.shotSet = {
-    time: 50,
+    time: 10,
     speed: 400,
     width: 15,
     length: 5,
     r: 2,
     color: "red",
-    anc:    new Vector(new Dot(star.translation.x + star.outerRadius / 2, star.translation.y),
-            new Dot(star.translation.x + star.outerRadius / 2 + 5, star.translation.y),
-            new Dot(star.translation.x + star.outerRadius / 2, star.translation.y))
+    anc: new Vector(new Dot(star.translation.x + star.outerRadius / 2, star.translation.y),
+        new Dot(star.translation.x + star.outerRadius / 2 + 5, star.translation.y),
+        new Dot(star.translation.x + star.outerRadius / 2, star.translation.y))
 }
-star.shotSet.anc.createOn(deTwo,{show:true,group:UI})
+star.shotSet.anc.createOn(deTwo, {
+    show: true,
+    group: UI
+})
 star.anchorPnts.last.animating = true
 star.anchorPnts.cur = star.anchorPnts.last
 star.anchorPnts.scaleInc = 0.1
 star.anchorPnts.r = 20
-star.tick = function () {
+star.tick = function (frameCount) {
     star.ancVec.lockOn(vec)
     star.shotSet.anc.lockOn(vec)
     star.shotSet.anc.update()
@@ -110,8 +113,25 @@ star.tick = function () {
         }
         cur.scale += cur.scaleInc
     }
+    star.partArr.tick(frameCount)
 }
 star.shotParticles = deTwo.makeGroup()
+star.partArr = new Two.Utils.Collection()
+star.partArr.tick = function (frameCount) {
+    star.partArr.forEach((el) => {
+        let part = el,
+            a = part.rotation,
+            vx = cos(a) * star.shotSet.speed / frames,
+            vy = sin(a) * star.shotSet.speed / frames
+        part.translation.x += vx
+        part.translation.y += vy
+        if (checkOut(part)) {
+            star.shotParticles.remove(part)
+            star.partArr.splice(star.partArr.indexOf(part), 1)
+        }
+    })
+}
+
 star.shotParticles.id = "shotPart"
 
 background.add(star.shotParticles)
@@ -122,22 +142,13 @@ star.shotInt = setInterval(() => {
         pos = star.translation,
         particle = deTwo.makeRoundedRectangle(svec.pf.x, svec.pf.y, set.width, set.length, set.r)
     star.shotParticles.add(particle)
+    star.partArr.push(particle)
     particle.rotation = svec.angle
     particle.fill = set.color
-    particle.tick = function () {
-        let part = particle,
-            a = part.rotation,
-            vx = cos(a) * set.speed / frames,
-            vy = sin(a) * set.speed / frames
-        part.translation.x += vx
-        part.translation.y += vy
-        if(checkOut(part)) star.shotParticles.remove(part)
-    }
-    deTwo.bind("update", particle.tick)
 }, star.shotSet.time)
 let frames = 60
 
-rect.tick = function () {
+rect.tick = function (frameCount) {
     let angle = vec.angle,
         vx = cos(angle) * speed / frames,
         vy = sin(angle) * speed / frames
@@ -151,10 +162,26 @@ rect.tick = function () {
         rect.moving = false
     }
     rotateAnchorPntsArrOf(rect)
+    rect.trail.tick(frameCount)
 }
 
 
 rect.trail = new Two.Utils.Collection()
+rect.trail.deleteArr = []
+rect.trail.tick = function (frameCount) {
+    rect.trail.forEach((el) => {
+        el.tick(frameCount)
+        if (rect.trail.length > rect.trail.maxLength) {
+            let elem = rect.trail.shift()
+            elem.deleting=true
+            rect.trail.deleteArr.push(elem) 
+        }
+    })
+    rect.trail.deleteArr.forEach((el)=>{
+        el.tick(frameCount)
+        if(el.scale<=0)rect.trail.deleteArr.splice(rect.trail.deleteArr.indexOf(el),1)
+    })
+}
 rect.trail.maxLength = 5
 rect.spawnTrail = setInterval(function () {
     if (rect.moving) {
@@ -163,41 +190,30 @@ rect.spawnTrail = setInterval(function () {
             sColor = new Two.Stop(0.5, getRandCSSColor(), 0.7),
             tColor = new Two.Stop(1, getRandCSSColor(), 1),
             grad = deTwo.makeLinearGradient(-10,
-            -5,
-            10,
-            5, fColor, sColor, tColor)
+                -5,
+                10,
+                5, fColor, sColor, tColor)
         newPart.fill = grad
         newPart.grad = grad
         newPart.noStroke()
         newPart.up = true
         newPart.scaleInc = 0.02
         newPart.deleting = false
-
-        deTwo.bind("update", function (frameCount) {
-            if (!newPart.deleting) {
-                if (newPart.scale > 2) {
-                    newPart.scaleInc *= -1
+        newPart.tick = function (frameCount) {
+            if (!this.deleting) {
+                if (this.scale > 2) {
+                    this.scaleInc *= -1
                 }
-                if (newPart.scale < 1) {
-                    newPart.scaleInc *= -1
+                if (this.scale < 1) {
+                    this.scaleInc *= -1
                 }
-                newPart.scale += newPart.scaleInc
+                this.scale += this.scaleInc
+            }else{
+                this.scale -= Math.abs(this.scaleInc)
             }
-        })
-
+        }
         rect.trail.push(newPart)
         background.add(newPart)
-        if (rect.trail.length > rect.trail.maxLength) {
-            let elem = rect.trail.shift()
-            elem.deleting = true
-            deTwo.bind("update", function (fc) {
-                elem.scale -= Math.abs(elem.scaleInc)
-                if (elem.scale < 0) {
-                    deTwo.remove(elem.grad)
-                    background.remove(elem)
-                }
-            })
-        }
     }
 }, 100)
 
@@ -207,10 +223,13 @@ testCirc.tick = function () {
     testCirc.translation.x = testCirc.p.x
     testCirc.translation.y = testCirc.p.y
 }
-// testRotVec.createOn(deTwo,{show:true,group:UI})
-testRotVec.tick = function(){
-    rotateVecFrom(centerP,testRotVec,0.02)
-    // testRotVec.update()
+testRotVec.createOn(deTwo, {
+    show: true,
+    group: UI
+})
+testRotVec.tick = function () {
+    rotateVecFrom(centerP, testRotVec, 0.02)
+    testRotVec.update()
 }
 
 function changeAnchorAngle() {
@@ -218,12 +237,14 @@ function changeAnchorAngle() {
     let angle = vec.angle
     rect.rotation = angle
 }
+
 function checkOut(elem) {
-    return(elem.translation.x<0||elem.translation.y<0||elem.translation.x>aWidth||elem.translation.y>aHeight)
+    return (elem.translation.x < 0 || elem.translation.y < 0 || elem.translation.x > aWidth || elem.translation.y > aHeight)
 }
 let statusBox = document.getElementById("statusDiv")
 statusBox.target = star.shotSet.anc
-function setStatusTarget(targ){
+
+function setStatusTarget(targ) {
     statusBox.target = targ
 }
 let speedFactor = 10,
@@ -235,7 +256,7 @@ deTwo.bind("update", rect.tick)
 deTwo.bind("update", testCirc.tick)
 deTwo.bind("update", star.tick)
 deTwo.bind("update", testRotVec.tick)
-deTwo.bind("update", (frameCount)=>{
+deTwo.bind("update", (frameCount) => {
     statusBox.update(statusBox.target)
 })
 svg.addEventListener("mousemove", changeAnchorAngle)
@@ -254,7 +275,7 @@ svg.addEventListener("wheel", function (e) {
 svg.addEventListener("mousemove", function (e) {
     cPos.checkPos(e, this)
 })
-statusBox.update = function (vectr){
+statusBox.update = function (vectr) {
     let obj = {
         first: `x:\t${Math.round(vectr._pnts.f.x)} \ty:\t${Math.round(vectr._pnts.f.y)}`,
         second: `x:\t${Math.round(vectr._pnts.s.x)} \ty:\t${Math.round(vectr._pnts.s.y)}`,
@@ -263,7 +284,7 @@ statusBox.update = function (vectr){
         vY: Math.round(vectr.y),
     }
     statusBox.innerHTML = ""
-    for(let key in obj){
+    for (let key in obj) {
         statusBox.innerHTML += key + ":\t" + obj[key] + "\n"
     }
     // console.log(this.innerHTML);
